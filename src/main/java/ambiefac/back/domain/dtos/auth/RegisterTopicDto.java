04884ee589e2ee85b1809d21eaa1a97cceee18a4
estsban11 <estsban11@gmail.com>
2023-11-11 19:25:20 -0500

    Add courses entities

--- src/main/java/ambiefac/back/domain/dtos/auth/RegisterTopicDto.java
+++ src/main/java/ambiefac/back/domain/dtos/auth/RegisterTopicDto.java
@@ -12,5 +12,19 @@ public class RegisterTopicDto {
     @NotNull()
     private String time;
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
 }
